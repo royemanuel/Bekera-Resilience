b9c3ea08e177f00930df90ecfdbf7af74f1ca02e
Roy N Emanuel II <roy.emanuel@gmail.com>
2015-09-09 19:50:04 -0400

    Fd vs Rho
    First working shiny app with Fd as the x axis and Rho as the y axis. Math may be off, but the graph is working.

--- server.R
+++ server.R
@@ -2,8 +2,27 @@
 library(shiny)
 library(ggplot2)
 # This is a calculation for the Bekera resilience metric rho
-rho <- function(slackTime, tStar, tF, Fi, Fd, Fr){
+rho <- function(slackTime, tStar, tF, a, Fi, Fd, Fr){
     if (tF >= tStar){
-        speedFactor <- (slackTime/tStar)*
+        speedFactor <- (slackTime/tStar)*exp(a * (tF - tStar))
+    } else {
+        speedFactor <- (slackTime/tStar)
     }
+    speedFactor * Fr * Fd / (Fi)^2
 }
+shinyServer(function(input, output){
+    output$Fd.v.rho <- renderPlot({
+        p <- ggplot(data.frame(x=c(0, 1.5)), aes(x)) +
+            stat_function(fun=function(x)rho(slackTime=input$slackTime,
+                              tStar=input$tStar,
+                              tF=input$tF,
+                              a=input$a,
+                              Fd=x,
+                              Fi=input$Fi,
+                              Fr=input$Fr))
+        p <- p + scale_x_continuous("Functionality after Disturbance") +
+            scale_y_continuous("Resilience Factor")
+        print(p)
+    })
+    }
+            )
--- ui.R
+++ ui.R
@@ -3,25 +3,23 @@ shinyUI(fluidPage(
     titlePanel("Bekera Resilience Factor (Bekera 2014)"),
     sidebarLayout(
         sidebarPanel(
-            sliderInput("Slack Time", "Time in Years:", min=0, max=10,
+            sliderInput("slackTime", "Time in Years:", min=0, max=10,
                         value=1, step=.5),
-            sliderInput("Time to Complete Initial Recovery Actions", min=0
+            sliderInput("tStar","Time to Complete Initial Recovery Actions", min=0,
                         max=20, value=5, step=0.5),
-            sliderInput("Time to Final Recovery", min=0, max=20, value=10,
+            sliderInput("tF", "Time to Final Recovery", min=0, max=20, value=10,
                         step=0.5),
-            sliderInput("Functionality prior to Disturbance", min=0, max=1.5,
+            sliderInput("a", "Resilience Decay", min=0, max=1.5,
                         value=1.0, step=0.05),
-            sliderInput("Functionality after Disturbance", min=0, max=1.5,
+            sliderInput("Fi", "Functionality prior to Disturbance", min=0, max=1.5,
                         value=0, step=0.05),
-            sliderInput("Functionality after Recovery", min=0, max=1.5,
-                        value=1.0, step=0.05)
-            sliderInput("Functionality after Recovery", min=0, max=1.5,
-                        value=1.0, step=0.05)
-            sliderInput("Functionality after Recovery", min=0, max=1.5,
+            sliderInput("Fd", "Functionality after Disturbance", min=0, max=1.5,
+                        value=0.5, step=0.05),
+            sliderInput("Fr", "Functionality after Recovery", min=0, max=1.5,
                         value=1.0, step=0.05)
         ),
         mainPanel(
-            plotOutput("rho")
+            plotOutput("Fd.v.rho")
         )
     )
 ))
